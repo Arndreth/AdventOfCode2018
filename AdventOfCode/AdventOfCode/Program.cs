4f7e506a237551060b2c7beee7ab44826ec5cbb6
Andrew Roper <contact@andrewroper.co.uk>
2018-12-03 00:02:16 +0000

    - Initial Commit - Day 1, part's 1 and 2 combined into one script.

---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var programInput = File.ReadAllLines("Resources/input.txt");
+            List<int> seenFrequencies = new List<int>() {0};
+            int finalOutput = 0, firstPassSummation;
+            char sign;
+            int value;
+            bool bFound = false, bFirstLoop = true;
+            for (int i = 0; i < programInput.Length; ++i)
+            {
+                sign = programInput[i][0];
+                value = int.Parse(programInput[i].Substring(1));
+
+                finalOutput += (sign.Equals('+') ? value : -value);
+                if (seenFrequencies.Contains(finalOutput))
+                {
+                    Console.WriteLine("\n---");
+                    Console.WriteLine($"Duplicate Frequency Found: {finalOutput}");
+                    break;
+                }
+
+                seenFrequencies.Add(finalOutput);
+                Console.Write($"\rFrequencies Scanned: {seenFrequencies.Count}");
+
+                //Reset the loop
+                if (i == programInput.Length - 1)
+                {
+                    //Store part one answer
+                    if (bFirstLoop)
+                    {
+                        firstPassSummation = finalOutput;
+                        bFirstLoop = false;
+                    }
+
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine($"Part One Answer: {firstPassSummation}");
+
+            Console.ReadKey();
+        }
+    }
+}
